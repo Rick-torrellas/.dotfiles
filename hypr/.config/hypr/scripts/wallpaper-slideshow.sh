3cc7606fb13038515e3cfbbc9fb967938cdbb533
Rick-torrellas <ricardo.torrejas@gmail.com>
2025-08-03 18:06:07 -0400

    tercer commit

--- hypr/.config/hypr/scripts/wallpaper-slideshow.sh
+++ hypr/.config/hypr/scripts/wallpaper-slideshow.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
+echo "Starting wallpaper slideshow..."
 # Define la variable WALLPAPER_DIR con el primer argumento pasado al script
 # Si no se proporciona un argumento, usa la ruta por defecto
 if [ -z "$1" ]; then
+    echo "No directory specified, using default: $HOME/.local/share/wallpapers/"
     WALLPAPER_DIR="$HOME/.local/share/wallpapers/"
 else
+echo "Using specified directory: $1"
     WALLPAPER_DIR="$1"
 fi
 
@@ -17,18 +19,20 @@ for format in "${formats[@]}"; do
     find_options+=" -o -name \"*.$format\""
 done
 
-# Elimina el primer '-o' que sobra
-find_options=${find_options:4}
+formats=("jpg" "jpeg" "png" "gif")
 
 while true; do
-    # Busca una imagen aleatoria en el directorio con los formatos especificados
-    random_wallpaper=$(find "$WALLPAPER_DIR" -type f \( $find_options \) | shuf -n 1)
+    find_args=()
+    for format in "${formats[@]}"; do
+        find_args+=( -o -iname "*.${format}" )
+    done
+    # Elimina el primer -o
+    find_args=("${find_args[@]:1}")
 
-    # Si se encontró una imagen, llama a swww para aplicarla
+    random_wallpaper=$(find "$WALLPAPER_DIR" -type f \( "${find_args[@]}" \) | shuf -n 1)
+    echo "Selected wallpaper: $random_wallpaper"
     if [[ -n "$random_wallpaper" ]]; then
-        swww img "$random_wallpaper" --transition-type fade # TODO: colocar el efecto de la transicion en una variable para poder cambiarlo con un argumento
+        swww img "$random_wallpaper" --transition-type fade
     fi
-
-    # Espera 15 minutos (900 segundos)
     sleep 900
 done
